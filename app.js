368f27727f2bfd5274bd66042c5eb2029d8d7d6b
wonderfulkasey <kaseyarnold15@gmail.com>
2021-06-21 13:55:20 -0400

    added some basic js and edited submit buttons

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById('submitPets').addEventListener('click', showConfirm);
+document.getElementById('submitChildren').addEventListener('click', showConfirm);
+document.getElementById('restart').addEventListener('click', showConfirm);
+document.getElementById('close').addEventListener('click', showConfirm);
